b0346d49691a0a6de60b75894a0ddfb4a5c10f0e
AntonVoronezh <pstut@mail.ru>
2018-12-11 21:11:59 +0300

    add

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,8 +1,4 @@
-console.log('kkfff111');
-const convert = items => items.reduce((acc, [key, value]) =>
-  ({ ...acc, [key]: value instanceof Array ? convert(value) : value }), {});
-  
-  
+
 // // начало карты
 ymaps.ready(function () {
     var myMap = new ymaps.Map('map', {
@@ -31,35 +27,154 @@ ymaps.ready(function () {
                 // Смещение левого верхнего угла иконки относительно
                 // её "ножки" (точки привязки).
                 iconImageOffset: [-1, -1]
-            }),
-
-        myPlacemarkWithContent = new ymaps.Placemark([51.689497, 39.176869], {
-            hintContent: 'Собственный значок метки с контентом',
-            balloonContent: 'А эта — новогодняя',
-            iconContent: '12'
-        }, {
-                // Опции.
-                // Необходимо указать данный тип макета.
-                iconLayout: 'default#imageWithContent',
-                // Своё изображение иконки метки.
-                iconImageHref: '.img/logo/logotype-map.svg',
-                // Размеры метки.
-                iconImageSize: [226, 30],
-                // Смещение левого верхнего угла иконки относительно
-                // её "ножки" (точки привязки).
-                iconImageOffset: [-24, -24],
-                // Смещение слоя с содержимым относительно слоя с картинкой.
-                iconContentOffset: [15, 15],
-                // Макет содержимого.
-                iconContentLayout: MyIconContentLayout
             });
+
+       
 
     myMap.geoObjects
         .add(myPlacemark)
-        .add(myPlacemarkWithContent);
 });
 // конец карты
 
+const getMobMenu = document.querySelector('.mob-menu');
+const getOpenMobMenu = document.querySelector('.mobile-menu');
+const getUserLink = document.querySelectorAll('.user__link');
+
+getMobMenu.addEventListener('click', closeMobMenu);
+
+function closeMobMenu() {
+  getMobMenu.classList.add('mob-menu-hide')
+};
+
+getOpenMobMenu.addEventListener('click', openMobMenu);
+
+function openMobMenu() {
+  getMobMenu.classList.remove('mob-menu-hide')
+};
+
+
+
+
+const getNewsShowBtn = document.querySelector('.news-show');
+const getHideNewsLists = document.querySelectorAll('.news__list--bottom');
+// const getShowNewsLists = document.querySelectorAll('.news__list--show');
+
+getNewsShowBtn.addEventListener('click', funcNewsShow);
+
+function funcNewsShow() {
+    getHideNewsLists.forEach(elem => {
+        elem.classList.toggle('news__list--show')
+    });
+    getNewsShowBtn.innerHTML = 'спрятать';
+    getNewsShowBtn.removeEventListener('click', funcNewsShow);
+    getNewsShowBtn.addEventListener('click', funcNewsHide);
+};
+
+function funcNewsHide() {
+   
+    getHideNewsLists.forEach(elem => {
+        elem.classList.toggle('news__list--show')
+    });
+    
+    getNewsShowBtn.innerHTML = 'показать все';
+    getNewsShowBtn.removeEventListener('click', funcNewsHide);
+    getNewsShowBtn.addEventListener('click', funcNewsShow);
+};
+
+// начало кода модальное окно отзывов
+let text = '<br><br><strong>СЕКРЕТЫ МЯГКОЙ И ПОСЛУШНОЙ БОРОДЫ</strong> \
+<br><br>1. ГОРЯЧИЙ ДУШ. ПОВЫШЕННАЯ ВЛАЖНОСТЬ И ПАР, РАСПАРЯТ КОЛЮЧИЕ ВОЛОСКИ ВАШЕЙ БОРОДЫ. ПОСЛЕ ДУША, ВОСПОЛЬЗУЙТЕСЬ УВЛАЖНЯЮЩИМ МАСЛОМ (ЛЮБЫМ, КОТОРОЕ У ВАС ЕСТЬ ИЛИ СПЕЦИАЛЬНЫМ ДЛЯ БОРОДЫ), НАКРОЙТЕ ЛИЦО МАХРОВЫМ ПОЛОТЕНЦЕМ НА 10-15 МИНУТ И СДЕЛАЙТЕ ЛЕГКИЙ МАССАЖ.\
+<br><br>2. НЕ МОЙТЕ БОРОДУ КАЖДЫЙ ДЕНЬ!!!ОТ ЕЖЕДНЕВНОГО МЫТЬЯ,ВОЛОСЫ НА БОРОДЕ СТАНОВЯТСЯ ЕЩЕ ЖЕСТЧЕ. МОЖНО МЫТЬ ЧЕРЕЗ ДЕНЬ,ЕСЛИ ЭТО КРАЙНЕ НЕОБХОДИМО, ТОГДА ПРИРОДНЫЕ МАСЛА,КОТОРЫЕ ДЕЛАЮТ ВОЛОСЫ ПОСЛУШНЫМИ УСПЕЮТ НАКОПИТЬСЯ.\
+<br><br>3. КОНДИЦИОНЕР ДЛЯ ВОЛОС ИЛИ СПЕЦИАЛЬНЫЙ КОНДИЦИОНЕР ДЛЯ БОРОДЫ. ДАННОЕ СРЕДСТВО ПОМОЖЕТ УПРАВЛЯТЬ ВОЛОСАМИ И СДЕЛАТЬ ИХ МАКСИМАЛЬНО ПОДАТЛИВЫМИ.';
+
+const getCommentsSelectors = document.querySelectorAll(".news__more-link");
+const successOverlay = createOverlay(" <img src=\"./img/news/news1.jpg\">" + text);
+// console.log(getCommentsSelectors.length);
+
+
+for (let i = 0; i < getCommentsSelectors.length; i += 1) {
+    getCommentsSelectors[i].addEventListener("click", function (event) {
+        event.preventDefault();
+        document.body.appendChild(successOverlay);
+    });
+}
+
+function createOverlay(content) {
+    const overlayElement = document.createElement("div");
+    overlayElement.classList.add("overlay");
+  
+    const template = document.querySelector("#overlayTemplate");
+    overlayElement.innerHTML = template.innerHTML;
+  
+    const closeElement = overlayElement.querySelector(".close");
+    closeElement.addEventListener("click", function(event) {
+        event.preventDefault();
+      document.body.removeChild(overlayElement);
+    });
+  
+    const contentElement = overlayElement.querySelector(".content");
+    contentElement.innerHTML = content;
+  
+    return overlayElement;
+  }
+
+
+
+const left = document.querySelector(".slider__left");
+const right = document.querySelector(".slider__right");
+const items = document.querySelector("#items");
+const sliderDotsAll = document.querySelectorAll('.slider__dots-item');
+const sliderDotsDiv = document.querySelector('.slider__dots');
+const item = items.querySelectorAll(".elem");
+
+const width = 100;
+let count = 0;
+
+right.addEventListener("click", function () {
+
+    if (count <= item.length - 2) {
+        count += 1;
+
+        makePosition(count);
+
+        sliderDotsAll.forEach(elem => elem.classList.remove('slider__dots-item--active'));
+        sliderDotsAll[count].classList.add('slider__dots-item--active');
+    }
+});
+
+left.addEventListener("click", function () {
+
+     if (count !== 0) {
+        count -= 1;
+
+        makePosition(count);
+
+        sliderDotsAll.forEach(elem => elem.classList.remove('slider__dots-item--active'));
+        sliderDotsAll[count].classList.add('slider__dots-item--active');
+    }
+});
+
+sliderDotsDiv.addEventListener('click', changeActiveDots);
+
+function changeActiveDots(event) {
+
+    sliderDotsAll.forEach(elem => elem.classList.remove('slider__dots-item--active'));
+    
+
+
+    event.target.classList.add('slider__dots-item--active');
+
+    sliderDotsAll.forEach((elem, num) => {
+        if(elem.classList.contains('slider__dots-item--active'))
+        makePosition(num);
+    } );
+
+};
+
+function makePosition(argCount) {
+    position = width * argCount;
+    return items.style.marginLeft = '-' + position + '%';
+};
 
 let player;
 
