a3c445cd99acfc6f2a140b9c4ba956c7862979a0
AntonVoronezh <pstut@mail.ru>
2018-12-11 21:13:43 +0300

    add v

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -125,6 +125,32 @@ close.addEventListener("click", function (evt) {
   popup.classList.remove("modal-error");
 });
 
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  if (!login.value || !password.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    localStorage.setItem("login", login.value);
+  }
+  console.log(login.value, password.value);
+
+  const successOverlay3 = createOverlay(`Вот тут ваши данные (${login.value}:${password.value}) будут куда-то отправлены`);
+  document.body.appendChild(successOverlay3);
+  
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
+
 
 const getNewsShowBtn = document.querySelector('.news-show');
 const getHideNewsLists = document.querySelectorAll('.news__list--bottom');
